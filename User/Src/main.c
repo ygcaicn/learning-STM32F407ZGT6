5c6b223b7d67a14f1cb8e5392a33f9815747daa1
C:/Program Files/Git <ygcaicn@gmail.com>
2016-05-29 00:58:06 +0800

    first commit PWM

--- User/Src/main.c
+++ User/Src/main.c
@@ -2,48 +2,29 @@
 #include "delay.h"
 #include "usart.h"
 #include "led.h"
-#include "beep.h"
-#include "key.h"
+#include "pwm.h"
+
 
 int main(void)
 { 
- 
-	u8 t;
-	u8 len;	
-	u16 times=0;  
+	u16 led0pwmval=0;    
+	u8 dir=1;
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
-	delay_init(168);		//延时初始化 
-	uart_init(115200);	//串口初始化波特率为115200
-	LED_Init();		  		//初始化与LED连接的硬件接口  	
-	printf("\r\n/**************串口实验*****************/\r\n");
-	while(1)
+	delay_init(168);  //初始化延时函数
+	uart_init(115200);//初始化串口波特率为115200
+ 	TIM14_PWM_Init(1000-1,84-1);	//84M/84=1Mhz的计数频率,重装载值500，所以PWM频率为 1M/500=2Khz.     
+   while(1) //实现比较值从0-300递增，到300后从300-0递减，循环
 	{
+ 		delay_ms(5);	 
+		if(dir)
+			led0pwmval++;//dir==1 led0pwmval递增
+		else 
+			led0pwmval--;	//dir==0 led0pwmval递减 
 		
-		if(USART_RX_STA&0x8000)
-		{					   
-			len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
-			printf("\r\n您发送的消息为:\r\n");
-			for(t=0;t<len;t++)
-			{
-				USART_SendData(USART1, USART_RX_BUF[t]);         //向串口1发送数据
-				while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);//等待发送结束
-			}
-			printf("\r\n\r\n");//插入换行
-			USART_RX_STA=0;
-		}else
-		{
-			times++;
-			if(times%5000==0)
-			{
-				printf("\r\n/**************串口实验*****************/\r\n");
-			}
-			
-			
-			if(times%200==0)printf("请输入数据，回车键结束\r\n");
-			delay_ms(10); 
-			if(times%30==0)LED0=!LED0;//闪烁LED,提示系统正在运行.
-			delay_ms(10);   
-		}
+ 		if(led0pwmval>800)dir=0;//led0pwmval到达300后，方向为递减
+		if(led0pwmval==0)dir=1;	//led0pwmval递减到0后，方向改为递增
+ 
+		TIM_SetCompare1(TIM14,led0pwmval);	//修改比较值，修改占空比
 	}
 }
 
